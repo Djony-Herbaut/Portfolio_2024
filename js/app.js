f695f26c9677d876f647ff8b4d27f9eab96603c2
Djony-Herbaut <djonyandrejovinac@gmail.com>
2023-09-13 17:52:34 +0200

    heighth  commit

--- js/app.js
+++ js/app.js
@@ -5,4 +5,17 @@ document.addEventListener("DOMContentLoaded", e => {
     console.log(elements.children);
     console.log(el.children);
     console.log(el.children[8].innerText = "Javascript");
+
+    // Sélectionnez le bouton de bascule de mode sombre
+    const toggleDarkModeButton = document.getElementById('toggle-dark-mode');
+
+    // Écoutez l'événement clic sur le bouton
+    toggleDarkModeButton.addEventListener('click', () => {
+        // Basculez la classe dark-mode sur le corps du document
+        document.body.classList.toggle('dark-mode');
+
+        // Basculez la classe d'icône pour le mode sombre
+        const iconMoon = toggleDarkModeButton.querySelector('.material-symbols-outlined');
+        iconMoon.classList.toggle('material-symbols-outlined');
+    });
 });
